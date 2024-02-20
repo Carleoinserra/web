88b306b736f3113d880fbaf2d7e54dc840270556
Carleoinserra <inserracarlo@gmail.com>
2024-02-20 16:49:37 +0100

    Update risto.php

--- risto.php
+++ risto.php
@@ -28,9 +28,60 @@ class ordine {
     }
 }
 
-$o1 = new ordine("Margherita", "patatine", "coca", 5, 3, 2);
+
+
+ $piz = $_GET["pizza"];
+  $con = $_GET["contorno"];
+$bib = $_GET["bibita"];
+
+
+$el1 = explode(".",$piz);
+
+$el2 = explode(".",$con);
+
+$el3 = explode(".",$bib);
+
+$o1 = new ordine($el1[0], $el2[0], $el3[0], $el1[1], $el2[1], $el3[1]);
+
 echo $o1 -> stampa();
 echo $o1 -> stampaTotale();
+
+try {
+  $db = new PDO('sqlite:database.sqlite');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $res = $db->exec(
+    "CREATE TABLE IF NOT EXISTS ordini (
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      pizza TEXT, 
+      contorno TEXT, 
+      bibita TEXT,
+      costo INTEGER
+    )"
+  );
+$piz1 = $o1 -> pizza;
+  
+    $con1 = $o1 -> contorno;
+    $bib1 = $o1 -> bibita;
+  $costo = $o1 -> stampaTotale();
+
+  // Query di inserimento preparata
+    $stmt = $db->prepare("INSERT INTO ordini (pizza, contorno, bibita, costo) VALUES (:pizza, :contorno, :bibita, :costo)");
+
+  // Esecuzione della query di inserimento
+    $stmt->execute(array(
+        ':pizza' => $piz1,
+        ':contorno' => $con1,
+        ':bibita' => $bib1,
+      ':costo' => $costo
+    ));}
+
+  catch (PDOException $e) {
+        echo "Errore durante l'inserimento del prodotoo: " . $e->getMessage();
+    }
+
+
+
 
 
 ?>
