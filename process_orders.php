aeb63becd6a8c32b7455d2287ed2ea4680d0bee9
Carleoinserra <inserracarlo@gmail.com>
2024-02-21 09:45:20 +0100

    Update process_orders.php

--- process_orders.php
+++ process_orders.php
@@ -1,9 +1,33 @@
 <?php
 
-echo("ciao");
 $result = ($_GET['decision']);
-for ($i = 0; $i <= count($result); $i++) {
-  echo($result[$i]);
-}
+var_dump($result);
+echo "<hr>";
+echo $result["2"];
+ 
+echo "<hr>";
 
-?>
+
+echo"<hr>";
+
+foreach ($result as $x) {
+  $temp = (explode(".",$x));
+  echo "$temp[0] <br>";
+  echo "$temp[1] <br>";
+  if ($temp[1] == "n"); {
+    try {
+    // Connessione a SQLite usando PDO
+    $db = new PDO('sqlite:database.sqlite');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   $stmt = $db->prepare("DELETE FROM ordini WHERE id = :order_id");
+$stmt->bindParam(':order_id', $temp[0]);
+$stmt->execute();
+   
+
+}
+  catch (PDOException $e) {
+    echo "Errore durante il recupero degli ordini: " . $e->getMessage();
+}
+  }
+}
